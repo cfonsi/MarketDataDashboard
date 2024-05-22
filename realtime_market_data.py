38fb78ce5cd38f1bc1fa550e3df5a75e1d621375
cfonsi <madeofclae@gmail.com>
2024-05-22 18:37:37 +0100

    update time interval added

--- realtime_market_data.py
+++ realtime_market_data.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 import json
 
-async def fetch_market_data(currency):
+async def fetch_market_data(currency,interval):
     
     BINANCE_WS_URL = f"wss://stream.binance.com:9443/ws/{currency}usdt@trade"
 
@@ -12,6 +12,7 @@ async def fetch_market_data(currency):
                 response = await websocket.recv()
                 data = json.loads(response)
                 print(f"Price of {currency.upper()}: {data['p']}")
+                await asyncio.sleep(interval)
             except websockets.ConnectionClosed:
                 print("Connection closed, retrying...")
                 await asyncio.sleep(1)
@@ -21,7 +22,7 @@ async def fetch_market_data(currency):
                 break
 
 def main():
-    asyncio.run(fetch_market_data('pyr'))
+    asyncio.run(fetch_market_data('pyr',5))
 
 if __name__ == "__main__":
     main()
