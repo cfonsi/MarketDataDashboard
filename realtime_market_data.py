ba2c1280cd860b5abe1a58612bcd528a0818af0d
cfonsi <madeofclae@gmail.com>
2024-05-22 18:32:40 +0100

    custom currency specification and closed connection handling

--- realtime_market_data.py
+++ realtime_market_data.py
@@ -2,21 +2,26 @@ import asyncio
 import websockets
 import json
 
-BINANCE_WS_URL = f"wss://stream.binance.com:9443/ws/btcusdt@trade"
+async def fetch_market_data(currency):
+    
+    BINANCE_WS_URL = f"wss://stream.binance.com:9443/ws/{currency}usdt@trade"
 
-async def fetch_market_data():
     async with websockets.connect(BINANCE_WS_URL) as websocket:
         while True:
             try:
                 response = await websocket.recv()
                 data = json.loads(response)
-                print(f"Price of BTC: {data['p']}")
+                print(f"Price of {currency.upper()}: {data['p']}")
+            except websockets.ConnectionClosed:
+                print("Connection closed, retrying...")
+                await asyncio.sleep(1)
+                continue
             except Exception as e:
                 print(f"Error: {e}")
                 break
 
 def main():
-    asyncio.run(fetch_market_data())
+    asyncio.run(fetch_market_data('pyr'))
 
 if __name__ == "__main__":
     main()
